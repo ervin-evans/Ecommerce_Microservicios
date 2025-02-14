63375cf8417a186ab2f0a618ec3ed54b9f73f9e8
Ervin Triano <ervintdeveloper@gmail.com>
2025-02-14 08:48:58 -0600

    Se agrego el gateway para el=nrutar y administrar el trafico de red hacia los microservicios

---
+++ gateway/src/test/java/com/evans/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evans.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
